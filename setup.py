0d82e2c9869da18809c58193c523e92e494409ab
Hannah Cushman Garland <hannah.cushman@datamade.us>
2024-08-29 16:09:31 -0500

    Built site for gh-pages

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name='muniscrapers',
-    version='1.0.0',
-    url='https://github.com/opencivicdata/scrapers-us-municipal',
-    packages=find_packages(),    
-    install_requires=['pupa', 'scraper-legistar'],
-)
