a70510c7e1393c6ed8334416d3253f6cfa9eb598
Lily Ye <lily.ye@datamade.us>
2026-09-25 16:45:03 -0500

    Add return types
    Remove type checking helper so that it's more obvious that we can guarantee
    the return type or an error.

--- lametro/factories.py
+++ lametro/factories.py
@@ -14,30 +14,34 @@ class OrganizationRef:
     def as_pseudo_id(self):
         return _make_pseudo_id(name=self.name)
     
-def checkOrgRef(arg):
-    if isinstance(arg, OrganizationRef):
-        return True
-    else:
-        raise TypeError(f"Keyword ${arg} expected argument of type OrganizationRef")
 
-def build_bill(*, from_organization: OrganizationRef, **kwargs):
-    if checkOrgRef(from_organization):
+def build_bill(*, from_organization: OrganizationRef, **kwargs) -> Bill:
+    if isinstance(from_organization, OrganizationRef):
         return Bill(
             from_organization=from_organization.as_dict,
             **kwargs
         )
+    else:
+        raise TypeError(f"Parameter from_organization expected argument of type OrganizationRef")
 
-def build_bill_action(*, organization: OrganizationRef, **kwargs):
-    if checkOrgRef(organization):
+
+def build_bill_action(*, organization: OrganizationRef, **kwargs) -> dict:
+    if isinstance(organization, OrganizationRef):
         return {
             "organization": organization.as_dict,
             **kwargs
         }
+    else:
+        raise TypeError(f"Parameter from_organization expected argument of type OrganizationRef")
 
-def add_related_entity_org(*, action, organization: OrganizationRef):
-    if checkOrgRef(organization):
+
+def add_related_entity_org(*, action, organization: OrganizationRef) -> None:
+    if isinstance(from_organization, OrganizationRef):
         action.add_related_entity(
             organization.name,
             "organization",
             entity_id=organization.as_pseudo_id,
         )
+    else:
+        raise TypeError(f"Parameter from_organization expected argument of type OrganizationRef")
+
